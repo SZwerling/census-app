331ae114af6c8b1bb7324cac7d44439f8ed46a6d
unknown <sazwerling@gmail.com>
2023-07-06 13:35:18 -0700

    delete unused comps

--- src/LineChart.jsx
+++ src/LineChart.jsx
@@ -2,8 +2,6 @@
 import React from "react";
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
-
-
 function LChart({data, location}) {
 
   const colors = ["black", "blue", "green", "red", "purple", "orange", "yellow"]
